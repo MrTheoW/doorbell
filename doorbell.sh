1e3166bbe2f325656557825060c3b7836dd319bc
Theo Willemse <t.willemse@gmail.com>
2019-11-08 00:46:07 +0100

    adjusted the script so it's more like the master

--- doorbell.sh
+++ doorbell.sh
@@ -1,18 +1,24 @@
-currentmonth="$(date +%m)"
-currentday="$(date +%d)"
+# Setting all variables
 
-medialocation="/home/doorbell/media"
-regular="/home/doorbell/media/regular"
-seasonal="/home/doorbell/media/seasonal"
-birthday="/home/doorbell/media/birthday"
-currentbell="$regular/$(ls $regular | shuf -n 1)"
-nodate="No special date today..."
-isthisaspecialdate=$nodate
+sounddevice="iec958:CARD=System,DEV=0"                                                  # The address of your soundcard
+medialocation="/home/doorbell/media"                                                    # The root of your media
+regular="$medialocation/regular"                                                        # The root of your regular doorbell tones
+seasonal="$medialocation/seasonal"                                                      # The root of your seasonal doorbell tones (with subfolder for christmass / halloween)
+birthday="$medialocation/birthday"                                                      # The root of your birthday doorbell tones
+nodate="No special date today..."                                                       # The default message (Actually doesn't do shit)
 
-if [ $currentmonth -eq 01 ]; then
-  if [ $currentday -gt 00 -a $currentday -lt 11 ]; then
-    currentbell="$seasonal/happynewyear.wav"
-    isthisaspecialdate="Happy Newyear!"
+# only change underneath if you know what you are doing!
+
+currentmonth="$(date +%m)"                                                              # Fetches the current month
+currentday="$(date +%d)"                                                                # Fetches the current day
+currentbell="$regular/$(ls $regular | shuf -n 1)"                                       # Sets the default doorbell by shuffeling the wav files in the regular folder
+#currentbell="$medialocation/test/beep.wav"                                             # Used for testing, Short beep sound that doesn't wake everyone in the house
+isthisaspecialdate=$nodate                                                              # Sets the default message as current message
+
+if [ $currentmonth -eq 01 ]; then                                                       # Checks the month
+  if [ $currentday -gt 00 -a $currentday -lt 11 ]; then                                 # Checks the day range
+    currentbell="$seasonal/happynewyear.wav"                                            # Sets the replacement doorbell
+    isthisaspecialdate="Happy Newyear!"                                                 # Changes the default message
   fi
 
 elif [ $currentmonth -eq 02 ]; then
@@ -52,11 +58,11 @@ elif [ $currentmonth -eq 10 ]; then
   if [ $currentday -eq 31 ]; then
     currentbell="$seasonal/halloween/$(ls $seasonal/halloween | shuf -n 1)"
     isthisaspecialdate="It's Halloween!"
+  elif [ $currentday -eq 01 ]; then
+    currentbell="$birthday/$(ls $birthday | shuf -n 1)"
+    isthisaspecialdate="It's Elissa's Birthday!"
   fi
 
-elif [ $currentday -eq 01 ]; then
-  currentbell="$birthday/$(ls $birthday | shuf -n 1)"
-  isthisaspecialdate="It's Elissa's Birthday!"
 
 elif [ $currentmonth -eq 11 ]; then
   isthisaspecialdate=$nodate
@@ -70,4 +76,4 @@ elif [ $currentmonth -eq 12 ]; then
 fi
 
 echo $isthisaspecialdate
-aplay --device iec958:CARD=System,DEV=0 $currentbell
+aplay --device $sounddevice $currentbell                                                # Use aplay to execute the the defined doorbells
