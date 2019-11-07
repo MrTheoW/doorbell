5c082eca88aadb70a79471790d729360b4939742
Theo Willemse <t.willemse@gmail.com>
2019-11-07 23:02:10 +0100

    Probably fixed a lot of if mistakes.

--- doorbell.sh
+++ doorbell.sh
@@ -16,62 +16,74 @@ if [ $currentmonth -eq 01 ]; then
     currentbell="$seasonal/happynewyear.wav"
     isthisaspecialdate="Happy Newyear!"
   fi
+fi
 
 elif [ $currentmonth -eq 02 ]; then
 #  if [ $currentday -eq 01 ]; then
 #  fi
   isthisaspecialdate=$nodate
+fi
 
 elif [ $currentmonth -eq 03 ]; then
   if [ $currentday -eq 11 ]; then
     currentbell="$birthday/$(ls $birthday | shuf -n 1)"
     isthisaspecialdate="It's Jane Doe's Birthday!"
   fi
+fi
 
 elif [ $currentmonth -eq 04 ]; then
   isthisaspecialdate=$nodate
+fi
 
 elif [ $currentmonth -eq 05 ]; then
 #  if [ $currentday -eq 01 ]; then
 #  fi
   isthisaspecialdate=$nodate
+fi
 
 elif [ $currentmonth -eq 06 ]; then
   if [ $currentday -eq 01 ]; then
     currentbell="$birthday/$(ls $birthday | shuf -n 1)"
     isthisaspecialdate="It's John Doe's Birthday!"
   fi
+fi
 
 elif [ $currentmonth -eq 07 ]; then
 #  if [ $currentday -eq 01 ]; then
 #  fi
   isthisaspecialdate=$nodate
+fi
 
 elif [ $currentmonth -eq 08 ]; then
 #  if [ $currentday -eq 01 ]; then
 #  fi
   isthisaspecialdate=$nodate
+fi
 
 elif [ $currentmonth -eq 09 ]; then
 #  if [ $currentday -eq 01 ]; then
 #  fi
   isthisaspecialdate=$nodate
+fi
 
 elif [ $currentmonth -eq 10 ]; then
   if [ $currentday -eq 31 ]; then
     currentbell="$seasonal/halloween/$(ls $seasonal/halloween | shuf -n 1)"
     isthisaspecialdate="It's Halloween!"
   fi
+fi
 
 elif [ $currentday -eq 01 ]; then
 #  if [ $currentday -eq 01 ]; then
 #  fi
   isthisaspecialdate=$nodate
+fi
 
 elif [ $currentmonth -eq 11 ]; then
 #  if [ $currentday -eq 01 ]; then
 #  fi
   isthisaspecialdate=$nodate
+fi
 
 elif [ $currentmonth -eq 12 ]; then
   if [ $currentday -gt 24 -a $currentday -lt 31 ]; then
