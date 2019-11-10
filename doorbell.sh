06e2a59e7f8d399414bb4e3566844ef1cbc52265
Theo Willemse <t.willemse@gmail.com>
2019-11-10 12:54:37 +0100

    added media and changed code

--- doorbell.sh
+++ doorbell.sh
@@ -5,6 +5,7 @@ medialocation="/home/doorbell/media"                                            
 regular="$medialocation/regular"                                                        # The root of your regular doorbell tones
 seasonal="$medialocation/seasonal"                                                      # The root of your seasonal doorbell tones (with subfolder for christmass / halloween)
 birthday="$medialocation/birthday"                                                      # The root of your birthday doorbell tones
+valentine="$medialocation/valentine"                                                    # The root of your valentine doorbell tones
 nodate="No special date today..."                                                       # The default message (Actually doesn't do shit)
 
 # only change underneath if you know what you are doing!
@@ -13,67 +14,45 @@ currentmonth="$(date +%m)"                                                      
 currentday="$(date +%d)"                                                                # Fetches the current day
 currentbell="$regular/$(ls $regular | shuf -n 1)"                                       # Sets the default doorbell by shuffeling the wav files in the regular folder
 #currentbell="$medialocation/test/beep.wav"                                             # Used for testing, Short beep sound that doesn't wake everyone in the house
-isthisaspecialdate=$nodate                                                              # Sets the default message as current message
 
 if [ $currentmonth -eq 01 ]; then                                                       # Checks the month
   if [ $currentday -gt 00 -a $currentday -lt 11 ]; then                                 # Checks the day range
-    currentbell="$seasonal/happynewyear.wav"                                            # Sets the replacement doorbell
-    isthisaspecialdate="Happy Newyear!"                                                 # Changes the default message
+    currentbell="$seasonal/newyear/$(ls $seasonal/newyear | shuf -n 1)"                 # Sets the replacement doorbell
   fi
 
 elif [ $currentmonth -eq 02 ]; then
-  isthisaspecialdate=$nodate
+  if [ $currentday -eq 11 ]; then
+    currentbell="$seasonal/valentine/$(ls $seasonal/valentine | shuf -n 1)"
+  fi
 
 elif [ $currentmonth -eq 03 ]; then
   if [ $currentday -eq 11 ]; then
     currentbell="$birthday/$(ls $birthday | shuf -n 1)"
-    isthisaspecialdate="It's Naomi's Birthday!"
   fi
-
-elif [ $currentmonth -eq 04 ]; then
-  isthisaspecialdate=$nodate
 
 elif [ $currentmonth -eq 05 ]; then
   if [ $currentday -eq 09 ]; then
     currentbell="$birthday/$(ls $birthday | shuf -n 1)"
-    isthisaspecialdate="It's Anita's Birthday!"
   fi
 
 elif [ $currentmonth -eq 06 ]; then
   if [ $currentday -eq 29 ]; then
     currentbell="$birthday/$(ls $birthday | shuf -n 1)"
-    isthisaspecialdate="It's Theo's Birthday!"
   fi
-
-elif [ $currentmonth -eq 07 ]; then
-  isthisaspecialdate=$nodate
-
-elif [ $currentmonth -eq 08 ]; then
-  isthisaspecialdate=$nodate
-
-elif [ $currentmonth -eq 09 ]; then
-  isthisaspecialdate=$nodate
 
 elif [ $currentmonth -eq 10 ]; then
   if [ $currentday -eq 31 ]; then
     currentbell="$seasonal/halloween/$(ls $seasonal/halloween | shuf -n 1)"
-    isthisaspecialdate="It's Halloween!"
   elif [ $currentday -eq 01 ]; then
     currentbell="$birthday/$(ls $birthday | shuf -n 1)"
-    isthisaspecialdate="It's Elissa's Birthday!"
   fi
 
-
-elif [ $currentmonth -eq 11 ]; then
-  isthisaspecialdate=$nodate
 
 elif [ $currentmonth -eq 12 ]; then
   if [ $currentday -gt 24 -a $currentday -lt 31 ]; then
     currentbell="$seasonal/christmas/$(ls $seasonal/christmas | shuf -n 1)"
-    isthisaspecialdate="Ho Ho Ho, It's Christmas time!"
   fi
 
 fi
 
-echo $isthisaspecialdate
 aplay --device $sounddevice $currentbell                                                # Use aplay to execute the the defined doorbells
